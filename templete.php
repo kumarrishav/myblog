f018d300461b4e648e1a0279246d743ee1fae21b
kumarrishav <rishav006@gmail.com>
2014-03-16 18:56:49 +0530

    templete for header,navigation and footer

---
+++ templete.php
@@ -0,0 +1,45 @@
+		<!--HEADER OF BLOG-->
+	<div class="container">
+		<h1><a href="#">Connect NIT</a></h1>
+		<hr class="colorgraph">
+			<div class="navbar">
+				<div class="navbar-inner">
+					<div class="container">
+						<ul class="nav">
+						<li class="active"><a href="main.php">Home</a></li>
+						<li><a href="post.php">About</a></li>
+						<li><a href="detail.php">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		<hr class="colorgraph">
+	</div>
+		<!--HEADER ENDS-->
+		
+		<!--RIGHT SIDE NAVIGATION-->
+				
+	<div class="container">
+		<div class="row">
+			<div id="archives">
+				<div class="span3" >
+					<ul class="nav nav-list">
+						<li class="nav-header">January</li>
+						<li class="active"><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li class="nav-header">February</li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+						<li><a href="#">ConnectNIT</a></li>
+					</ul>
+	        	</div>
+			</div>
+		</div>
+	</div>
+		<!--NAVIGATION ENDS-->
+	
---
+++ templete_footer.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<hr>
+		<div class="row">
+			<div class="span4">
+				<h4 class="muted text-center">Recent Activities</h4>
+				<p>In many scenarios web developers have the entire setup ready with them but they can’t proceed due to absence of the design of the project they are going to work on.</p>
+				<a href="#" class="btn"><i class="icon-user"></i> ConnectNIT</a>
+			</div>
+			<div class="span4">
+				<h4 class="muted text-center">About connectnit</h4>
+				<p> This process consumes a hell lot of time and is a serious wastage of time for a developer who needs his/her ideas to be implementing as quickly as possible. In such scenarios, Twitter Bootstrap comes to the rescue in the super-heroic fashion!.</p>
+				<a href="#" class="btn btn-success"><i class="icon-star icon-white"></i>ConnectNIT</a>
+			</div>
+			<div class="span4">
+				<h4 class="muted text-center">Reach Us</h4>
+				<p>In many scenarios web developers have the entire setup ready with them but they can’t proceed due to absence of the design of the project they are going to work on. They have to rely on the designers to complete the client side of their project.</p>
+				<a href="#" class="btn btn-info">ConnectNIT</a>
+			</div>
+		</div>
+
+			<!-- Copyright Area -->
+		<hr>
+		<div class="footer">
+			<p>&copy; 2014</p>
+		</div>
+	</div>
